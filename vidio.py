18e19a4201fa61c091dce876b4edcd3c3f502527
Ivanpradipta <79517923+Ivanpradipta@users.noreply.github.com>
2021-04-27 00:08:38 +0700

    Add files via upload

---
+++ vidio.py
@@ -0,0 +1,14 @@
+def Nama(a,b,c,d):
+    a={"nama":"Joanne"}
+    b={"pekerjaan":"pengangguran"}
+    c={"status":"jomblo"}
+    d={"Tempat Tinggal":"balekambang"}
+    f=a|b|c|d
+    print(f)
+
+a={"nama":"Joanne"}
+b={"pekerjaan":"pengangguran"}
+c={"status":"jomblo"}
+d={"Tempat Tinggal":"balekambang"}
+print(Nama(a,b,c,d))
+    
